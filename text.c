cb47f0170fb61ac095bb9ef20e149b627fa5a624
Pibil Francis <billfranc20@gmail.com>
2022-09-13 00:57:59 +0000

    adding comments

--- text.c
+++ text.c
@@ -1,7 +1,13 @@
+/*
+Author : Pibil Francis
+Purpose : This program prints out my name
+*/
+
 #include <stdio.h>
 
 int main()
 {
+    //This code is displying my name
     printf("Hi, my name is Pibil Francis Annan");
     return 0;
 }
